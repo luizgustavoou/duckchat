750890d1f5bc99a09c43bee4984ddc477b010ef3
luizgustavoou <luiz.umbelino@nuplam.ufrn.br>
2023-11-03 19:13:54 -0300

    feature(CardFriend): criar componente

---
+++ duckchat-front/src/components/CardFriend.tsx
@@ -0,0 +1,40 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
+
+export interface Friend {
+  name: string;
+  status: boolean;
+  image: string;
+}
+
+interface CardFriendProps {
+  friend: Friend;
+}
+
+export default function CardFriend(props: CardFriendProps) {
+  return (
+    <div className="flex py-3 px-4 items-center space-x-4 hover:bg-slate-600 cursor-pointer">
+      <>
+        <Avatar>
+          <AvatarImage className="w-12 rounded-full" src={props.friend.image} />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+        <div className="flex flex-col gap-1">
+          <div>{props.friend.name}</div>
+          <div>
+            {props.friend.status ? (
+              <div className="flex items-center gap-1">
+                <span>Ativo</span>{" "}
+                <div className="rounded-full w-3 h-3 bg-green-300 align-middle"></div>
+              </div>
+            ) : (
+              <div className="flex items-center gap-1">
+                <span>Inativo</span>{" "}
+                <div className="rounded-full w-3 h-3 bg-red-400 align-middle"></div>
+              </div>
+            )}
+          </div>
+        </div>
+      </>
+    </div>
+  );
+}
--- duckchat-front/src/components/Home.tsx
+++ duckchat-front/src/components/Home.tsx
@@ -1,47 +1,28 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
-import { Skeleton } from "./ui/skeleton";
+import CardFriend, { Friend } from "./CardFriend";
 
 export default function Home() {
-  const friends = [
+  const friends: Friend[] = [
     {
-      name: "joão",
+      name: "Joab",
       status: false,
+      image: "https://github.com/shadcn.png",
+    },
+    {
+      name: "Luiz",
+      status: true,
       image: "https://github.com/shadcn.png",
     },
   ];
 
   return (
     <div className="flex min-h-screen">
-      <div className="flex flex-col bg-secondary-foreground p-3 text-white gap-2 min-w-[16%]">
-        <div className="flex items-center space-x-4">
-          {friends.map((friend) => (
-            <>
-              <Avatar>
-                <AvatarImage className="w-12 rounded-full" src={friend.image} />
-                <AvatarFallback>CN</AvatarFallback>
-              </Avatar>
-              <div className="space-y-2">
-                <div>{friend.name}</div>
-                <div>
-                  {friend.status ? (
-                    <div className="flex items-center gap-1">
-                      <span>Ativo</span>{" "}
-                      <div className="rounded-full w-3 h-3 bg-green-300 align-middle"></div>
-                    </div>
-                  ) : (
-                    <div className="flex items-center gap-1">
-                      <span>Inativo</span>{" "}
-                      <div className="rounded-full w-3 h-3 bg-red-400 align-middle"></div>
-                    </div>
-                  )}
-                </div>
-              </div>
-            </>
-          ))}
-        </div>
+      <div className="flex flex-col bg-secondary-foreground  text-white gap-6 min-w-[16%]">
+        {friends.map((friend) => (
+          <CardFriend friend={friend}></CardFriend>
+        ))}
       </div>
 
-      <div className="text-white flex-1">sfdsdfsdf</div>
+      <div className="text-white flex-1"></div>
     </div>
   );
 }
