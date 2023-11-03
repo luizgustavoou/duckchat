01d17e14d57e737a44ecdfaf320fa0c6a68d281a
Joab Urbano <joab.uaraujo@gmail.com>
2023-11-03 18:43:09 -0300

    Criação do status do amigo

--- duckchat-front/src/components/Home.tsx
+++ duckchat-front/src/components/Home.tsx
@@ -2,39 +2,46 @@ import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
 import { Skeleton } from "./ui/skeleton";
 
 export default function Home() {
-
-  const friends = [{
-    'name': 'joão',
-    'status' : true,
-    'image' : 'https://github.com/shadcn.png'
-  }]
+  const friends = [
+    {
+      name: "joão",
+      status: false,
+      image: "https://github.com/shadcn.png",
+    },
+  ];
 
   return (
     <div className="flex min-h-screen">
       <div className="flex flex-col bg-secondary-foreground p-3 text-white gap-2 min-w-[16%]">
         <div className="flex items-center space-x-4">
-        {friends.map((friend) => (
-          <>
-            <Avatar>
-              <AvatarImage className="w-12 rounded-full" src={friend.image} />
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
+          {friends.map((friend) => (
+            <>
+              <Avatar>
+                <AvatarImage className="w-12 rounded-full" src={friend.image} />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
               <div className="space-y-2">
                 <div>{friend.name}</div>
                 <div>
-                  {friend.status ? <span>Ativo <div className="rounded-full w-3 h-3 bg-green-300"></div></span>: <span>Inativo</span>}
+                  {friend.status ? (
+                    <div className="flex items-center gap-1">
+                      <span>Ativo</span>{" "}
+                      <div className="rounded-full w-3 h-3 bg-green-300 align-middle"></div>
+                    </div>
+                  ) : (
+                    <div className="flex items-center gap-1">
+                      <span>Inativo</span>{" "}
+                      <div className="rounded-full w-3 h-3 bg-red-400 align-middle"></div>
+                    </div>
+                  )}
                 </div>
               </div>
-          </>
-        ))}
+            </>
+          ))}
         </div>
       </div>
 
-
-
-      <div className="text-white flex-1">
-        sfdsdfsdf
-      </div>
+      <div className="text-white flex-1">sfdsdfsdf</div>
     </div>
   );
 }
