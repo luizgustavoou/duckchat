f7a2338bc7ca3e417d122ccb8272541885e34ecb
luizgustavoou <luiz.umbelino@nuplam.ufrn.br>
2023-11-03 17:47:49 -0300

    feature: resetar css

--- duckchat-front/src/App.tsx
+++ duckchat-front/src/App.tsx
@@ -1,16 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 import Home from "./components/Home";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
+    <div className="min-h-screen bg-current m-0 p-0">
       <Home></Home>
-    </>
+    </div>
   );
 }
 
