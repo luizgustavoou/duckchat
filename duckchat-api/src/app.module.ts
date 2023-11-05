8abbb3aefd10d67daa188f8e285190df592f8f81
luizgustavoou <luiz.umbelino@nuplam.ufrn.br>
2023-11-05 20:59:17 -0300

    feature(chatgateway): criar websocket

--- duckchat-api/src/app.module.ts
+++ duckchat-api/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
-  imports: [],
+  imports: [ChatModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ duckchat-api/src/chat/chat.gateway.ts
@@ -0,0 +1,30 @@
+import { ConnectedSocket, MessageBody, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Socket, Server } from 'socket.io';
+
+@WebSocketGateway()
+export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
+
+  @WebSocketServer()
+  server: Server;
+
+  handleDisconnect(client: any) {
+    // implement...
+  }
+
+  handleConnection(client: any, ...args: any[]) {
+    console.log(`Client connected: ${client.id}`)
+
+
+    // implement...
+  }
+
+  @SubscribeMessage('message')
+  handleEvent(@MessageBody() data: any, @ConnectedSocket() client: Socket) {
+    this.server.emit("onMessage", {
+      msg: "New Message",
+      content: data
+    });
+
+
+  }
+}
