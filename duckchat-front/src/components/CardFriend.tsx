0aca70310b402b37b38ea5fc11d1caa3d8590486
luizgustavoou <luizgustavooumbelino@gmail.com>
2024-01-29 14:41:29 -0300

    mockar friendships no home

--- duckchat-front/src/components/CardFriend.tsx
+++ duckchat-front/src/components/CardFriend.tsx
@@ -1,27 +1,25 @@
+import { IFriendship } from "@/entities/IFriendship";
 import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
 
-export interface Friend {
-  name: string;
-  status: boolean;
-  image: string;
+interface ICardFriendProps {
+  friendship: IFriendship;
 }
 
-interface CardFriendProps {
-  friend: Friend;
-}
-
-export default function CardFriend(props: CardFriendProps) {
+export default function CardFriend({ friendship }: ICardFriendProps) {
   return (
     <div className="flex gap-3 py-3 px-4 items-center hover:bg-slate-600 cursor-pointer">
       <>
         <Avatar>
-          <AvatarImage className="w-12 rounded-full" src={props.friend.image} />
+          <AvatarImage
+            className="w-12 rounded-full"
+            src={friendship.friend.avatarURL}
+          />
           <AvatarFallback>CN</AvatarFallback>
         </Avatar>
         <div className="flex flex-col gap-1">
-          <div>{props.friend.name}</div>
-          <div>
-            {props.friend.status ? (
+          <div>{friendship.friend.firstName}</div>
+          {/* <div>
+            {friendship.status ? (
               <div className="flex items-center gap-1">
                 <span>Ativo</span>{" "}
                 <div className="rounded-full w-3 h-3 bg-green-300 align-middle"></div>
@@ -32,7 +30,7 @@ export default function CardFriend(props: CardFriendProps) {
                 <div className="rounded-full w-3 h-3 bg-red-400 align-middle"></div>
               </div>
             )}
-          </div>
+          </div> */}
         </div>
       </>
     </div>
--- duckchat-front/src/pages/Home/Home.tsx
+++ duckchat-front/src/pages/Home/Home.tsx
@@ -1,25 +1,38 @@
-import CardFriend, { Friend } from "../../components/CardFriend";
+import { IFriendship } from "@/entities/IFriendship";
+import CardFriend from "../../components/CardFriend";
 import Chat from "../../components/Chat";
 
 export default function Home() {
-  const friends: Friend[] = [
+  const friendships: IFriendship[] = [
     {
-      name: "Joab",
-      status: false,
-      image: "https://github.com/shadcn.png",
+      id: "88d8fd19-a014-43db-9d50-94c0d03898d5",
+      friend: {
+        id: "c4981d52-2ec3-47ab-9ede-3f110a6a76db",
+        username: "cleidiane",
+        password: "123",
+        firstName: "Cleidiane",
+        lastName: "Moura",
+        avatarURL: "https://github.com/shadcn.png",
+      },
     },
     {
-      name: "Luiz",
-      status: true,
-      image: "https://github.com/shadcn.png",
+      id: "952f2ea2-a997-44ac-92ec-50217bafc6d7",
+      friend: {
+        id: "c9f94b42-303e-410d-8635-c4bf6353b885",
+        username: "jorge",
+        password: "123",
+        firstName: "Jorge",
+        lastName: "Fernandes",
+        avatarURL: "https://github.com/shadcn.png",
+      },
     },
   ];
 
   return (
     <>
       <div className="flex flex-col text-white gap-6">
-        {friends.map((friend) => (
-          <CardFriend friend={friend}></CardFriend>
+        {friendships.map((friendship) => (
+          <CardFriend friendship={friendship} key={friendship.id}></CardFriend>
         ))}
       </div>
 
