42e2afc2318876b551edde408f6f045348384321
luizgustavoou <luizgustavooumbelino@gmail.com>
2024-01-29 15:10:09 -0300

    ajustar responsividade de chat

--- duckchat-front/src/components/Chat.tsx
+++ duckchat-front/src/components/Chat.tsx
@@ -1,6 +1,7 @@
 import { Avatar, AvatarImage, AvatarFallback } from "@radix-ui/react-avatar";
 import { Textarea } from "./ui/textarea";
 import { IFriendship } from "@/entities/IFriendship";
+import { IMessage } from "@/entities/IMessage";
 
 export default function Chat() {
   const friendship: IFriendship = {
@@ -15,13 +16,148 @@ export default function Chat() {
     },
   };
 
-  const mensage = {
-    message: "Garcia é otaku",
-    username: "Luiz",
-    createdAt: Date.now(),
-    avatar:
-      "https://images.vexels.com/media/users/3/308170/isolated/preview/6f83ccf0efa21e0a5a1864723411c8d2-pato-de-borracha-flutuante.png",
-  };
+  const messages: IMessage[] = [
+    {
+      id: "3b221db3-35b2-43bd-908e-ca2dc41a85b7",
+      createdAt: "2024-01-28T17:34:40.419Z",
+      updatedAt: "2024-01-28T17:34:40.433Z",
+      content: "msg atualizada kkk errei a palavra",
+      user: {
+        id: "64f7485f-9083-409e-ba2d-67a37429a399",
+        username: "luizgustavoou",
+        password: "123",
+        firstName: "gugu delicia",
+        lastName: "Umbelino",
+        avatarURL: "https://github.com/shadcn.png",
+      },
+    },
+    {
+      id: "6ebf89b3-110f-4bee-9d46-62eb111f8c37",
+      createdAt: "2024-01-28T17:40:27.480Z",
+      updatedAt: "2024-01-28T17:40:27.480Z",
+      content: "tudo bem?",
+      user: {
+        id: "64f7485f-9083-409e-ba2d-67a37429a399",
+        username: "luizgustavoou",
+        password: "123",
+        firstName: "gugu delicia",
+        lastName: "Umbelino",
+        avatarURL: "https://github.com/shadcn.png",
+      },
+    },
+    {
+      id: "a4adab8f-2ad8-45fd-a152-04a185b43267",
+      createdAt: "2024-01-28T19:40:09.854Z",
+      updatedAt: "2024-01-28T19:40:09.854Z",
+      content: "olá sou joao?",
+      user: {
+        id: "c9f94b42-303e-410d-8635-c4bf6353b885",
+        username: "jorge",
+        password: "123",
+        firstName: "Jorge",
+        lastName: "Fernandes",
+        avatarURL: "https://github.com/shadcn.png",
+      },
+    },
+    {
+      id: "edd4229d-ac4f-4eba-ad13-97a3634b45c1",
+      createdAt: "2024-01-28T19:41:13.374Z",
+      updatedAt: "2024-01-28T19:41:13.374Z",
+      content: "olá João, sou luiz?",
+      user: {
+        id: "64f7485f-9083-409e-ba2d-67a37429a399",
+        username: "luizgustavoou",
+        password: "123",
+        firstName: "gugu delicia",
+        lastName: "Umbelino",
+        avatarURL: "https://github.com/shadcn.png",
+      },
+    },
+    {
+      id: "0ef69890-1a42-46ac-9c47-a8e1a1eb7761",
+      createdAt: "2024-01-28T20:37:09.611Z",
+      updatedAt: "2024-01-28T20:37:09.611Z",
+      content: "teste1",
+      user: {
+        id: "64f7485f-9083-409e-ba2d-67a37429a399",
+        username: "luizgustavoou",
+        password: "123",
+        firstName: "gugu delicia",
+        lastName: "Umbelino",
+        avatarURL: "https://github.com/shadcn.png",
+      },
+    },
+    {
+      id: "25cf6313-3ee0-4da1-8456-25d5399b9bb2",
+      createdAt: "2024-01-28T21:45:27.314Z",
+      updatedAt: "2024-01-28T21:45:27.314Z",
+      content: "teste1",
+      user: {
+        id: "64f7485f-9083-409e-ba2d-67a37429a399",
+        username: "luizgustavoou",
+        password: "123",
+        firstName: "gugu delicia",
+        lastName: "Umbelino",
+        avatarURL: "https://github.com/shadcn.png",
+      },
+    },
+    {
+      id: "f799234d-96b4-4bc3-978a-98bdd4496599",
+      createdAt: "2024-01-29T14:05:16.294Z",
+      updatedAt: "2024-01-29T14:05:16.294Z",
+      content: "eai cara!",
+      user: {
+        id: "64f7485f-9083-409e-ba2d-67a37429a399",
+        username: "luizgustavoou",
+        password: "123",
+        firstName: "gugu delicia",
+        lastName: "Umbelino",
+        avatarURL: "https://github.com/shadcn.png",
+      },
+    },
+    {
+      id: "1578cc13-0db5-4034-bf79-c3679b4a5714",
+      createdAt: "2024-01-29T16:25:41.485Z",
+      updatedAt: "2024-01-29T16:25:41.485Z",
+      content: "eai cara!",
+      user: {
+        id: "64f7485f-9083-409e-ba2d-67a37429a399",
+        username: "luizgustavoou",
+        password: "123",
+        firstName: "gugu delicia",
+        lastName: "Umbelino",
+        avatarURL: "https://github.com/shadcn.png",
+      },
+    },
+    {
+      id: "5771f997-2f0e-48ff-b048-1f71dedc4f85",
+      createdAt: "2024-01-29T16:25:59.484Z",
+      updatedAt: "2024-01-29T16:25:59.484Z",
+      content: "eai cara!",
+      user: {
+        id: "64f7485f-9083-409e-ba2d-67a37429a399",
+        username: "luizgustavoou",
+        password: "123",
+        firstName: "gugu delicia",
+        lastName: "Umbelino",
+        avatarURL: "https://github.com/shadcn.png",
+      },
+    },
+    {
+      id: "4825f248-401a-4d1b-93b6-00d47da763dc",
+      createdAt: "2024-01-29T16:29:26.599Z",
+      updatedAt: "2024-01-29T16:29:26.599Z",
+      content: "eai cara!",
+      user: {
+        id: "64f7485f-9083-409e-ba2d-67a37429a399",
+        username: "luizgustavoou",
+        password: "123",
+        firstName: "gugu delicia",
+        lastName: "Umbelino",
+        avatarURL: "https://github.com/shadcn.png",
+      },
+    },
+  ];
 
   const chatName = "Luiz Gustavo";
 
@@ -39,23 +175,25 @@ export default function Chat() {
       </div>
 
       <div className="flex-1">
-        <div className="flex py-3 px-4 items-center gap-2 hover:bg-slate-600 cursor-pointer">
-          <>
+        {messages.map((messsage) => (
+          <div className="flex py-3 px-4 items-center gap-2 hover:bg-slate-600 cursor-pointer">
             <Avatar>
-              <AvatarImage className="w-12 rounded-full" src={mensage.avatar} />
+              <AvatarImage
+                className="w-12 rounded-full"
+                src={messsage.user.avatarURL}
+              />
               <AvatarFallback>CN</AvatarFallback>
             </Avatar>
-            <div className="flex flex-col gap-1">
-              <div>
-                {mensage.username}{" "}
-                <span className="text-sm text-muted-foreground">
-                  {mensage.createdAt}
-                </span>
-              </div>
-              <div>{mensage.message}</div>
+            <div>
+              <span className="mr-1">{messsage.user.firstName}</span>
+              <span className="text-sm text-muted-foreground">
+                {messsage.createdAt}
+              </span>
+
+              <p>{messsage.content}</p>
             </div>
-          </>
-        </div>
+          </div>
+        ))}
       </div>
 
       <Textarea
