f8bc496b2bc1da3fea5113a86bb02352bfffcc10
luizgustavoou <luizgustavooumbelino@gmail.com>
2024-02-07 00:16:29 -0300

    feature(user-slice): adicionar thunk update profile

--- duckchat-front/src/slices/user-slice.ts
+++ duckchat-front/src/slices/user-slice.ts
@@ -1,11 +1,14 @@
 // ler: https://blog.logrocket.com/using-typescript-redux-toolkit/
 
 import { IUser } from "@/entities/IUser";
-import { RootState } from "@/store";
-import { createSlice } from "@reduxjs/toolkit";
+import { IUpdateProfile } from "@/interfaces/IUpdateProfile";
+import { userService } from "@/services";
+import { AppDispatch, RootState } from "@/store";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 export interface UserState {
   user: IUser | null;
+  status: "idle" | "loading" | "success" | "error";
 }
 
 const initialState: UserState = {
@@ -18,12 +21,47 @@ const initialState: UserState = {
     about: "every little thing gonna be all right",
     avatarURL: "https://github.com/shadcn.png",
   },
+  status: "idle",
 };
+
+export const updateProfile = createAsyncThunk<
+  IUser,
+  IUpdateProfile,
+  {
+    dispatch: AppDispatch;
+    state: RootState;
+    rejectValue: string;
+  }
+>("user/updateprofile", async (data, thunkAPI) => {
+  try {
+    const res = await userService.updateProfile(data);
+
+    return res;
+  } catch (error: any) {
+    console.log(error);
+    return thunkAPI.rejectWithValue(
+      error?.message || "Ocorreu algum erro interno no servidor."
+    );
+  }
+});
 
 export const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(updateProfile.pending, (state, _) => {
+        state.status = "loading";
+      })
+      .addCase(updateProfile.fulfilled, (state, action) => {
+        state.user = action.payload;
+        state.status = "success";
+      })
+      .addCase(updateProfile.rejected, (state, _) => {
+        state.status = "error";
+      });
+  },
 });
 
 export const {} = userSlice.actions;
