22e10d580314a388deeb087c25f2dd1f8b46296d
luizgustavoou <luizgustavooumbelino@gmail.com>
2024-02-07 18:37:51 -0300

    refactore(AddFriend): usar o AppDialog

--- duckchat-front/src/components/AddFriend.tsx
+++ duckchat-front/src/components/AddFriend.tsx
@@ -1,14 +1,5 @@
 import { Input } from "@/components/ui/input";
 
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import { Check, Plus } from "lucide-react";
 import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
@@ -22,6 +13,7 @@ import {
   getAllNonFriendsUsers,
   nonFriendsUsersSelector,
 } from "../slices/non-friends-users-slice";
+import AppDialog from "./AppDialog";
 
 function AddFriend() {
   const dispatch = useAppDispatch();
@@ -69,8 +61,8 @@ function AddFriend() {
   const disableActions = nonFriendsStatus === "loading";
 
   return (
-    <Dialog>
-      <DialogTrigger asChild>
+    <AppDialog
+      trigger={
         <Button
           variant="outline"
           size="icon"
@@ -78,14 +70,10 @@ function AddFriend() {
         >
           <Plus className="h-4 w-4" />
         </Button>
-      </DialogTrigger>
-      <DialogContent className="sm:max-w-[600px]">
-        <DialogHeader>
-          <DialogTitle>Adicionar amigos</DialogTitle>
-          <DialogDescription>
-            Faça novas amizades para iniciar um novo bate-papo.
-          </DialogDescription>
-        </DialogHeader>
+      }
+      title={<p>Adicionar amigos</p>}
+      description={<p>Faça novas amizades para iniciar um novo bate-papo.</p>}
+      main={
         <div className="flex flex-col gap-4 py-4">
           <div className="flex items-center">
             {/* <Search width={16} height={16} /> */}
@@ -117,22 +105,22 @@ function AddFriend() {
               </div>
             ))}
         </div>
-        <DialogFooter>
-          <div className="w-full flex justify-between">
-            <p className="text-sm text-muted-foreground">
-              Selecione usuários para serem adicionados
-            </p>
-            <Button
-              type="submit"
-              onClick={handleSubmit}
-              disabled={disableActions}
-            >
-              Adicionar
-            </Button>
-          </div>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
+      }
+      footer={
+        <div className="w-full flex justify-between">
+          <p className="text-sm text-muted-foreground">
+            Selecione usuários para serem adicionados
+          </p>
+          <Button
+            type="submit"
+            onClick={handleSubmit}
+            disabled={disableActions}
+          >
+            Adicionar
+          </Button>
+        </div>
+      }
+    ></AppDialog>
   );
 }
 
