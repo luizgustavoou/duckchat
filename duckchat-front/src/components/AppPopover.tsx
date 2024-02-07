7da553e46c283272f4c192fbd036bf191b70cecf
luizgustavoou <luizgustavooumbelino@gmail.com>
2024-02-07 18:51:33 -0300

    feature(AppPopover): criar

---
+++ duckchat-front/src/components/AppPopover.tsx
@@ -0,0 +1,29 @@
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { ReactNode } from "react";
+
+export interface AppPopoverProps {
+  trigger: ReactNode;
+  content: ReactNode;
+  parentContentClassName?: string;
+}
+
+function AppPopover({
+  trigger,
+  content,
+  parentContentClassName,
+}: AppPopoverProps) {
+  return (
+    <Popover>
+      <PopoverTrigger asChild>{trigger}</PopoverTrigger>
+      <PopoverContent className={parentContentClassName}>
+        {content}
+      </PopoverContent>
+    </Popover>
+  );
+}
+
+export default AppPopover;
